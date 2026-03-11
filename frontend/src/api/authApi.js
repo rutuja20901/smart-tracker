c553cde7cb56cc57cc766b8f61b15c398684567e
rutuja20901 <rutujachavan20901@gmail.com>
2026-03-11 21:43:53 +0530

    update authapi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/auth"; 
+const API_URL = "https://expense-tracker-api.onrender.com/auth"; 
 // change port/path if your backend differs
 
 export const registerUser = (registerData) => {
